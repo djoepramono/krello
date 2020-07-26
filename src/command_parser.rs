671b3971b6f758a63a610461397f5deb2882edc8
Djoe Pramono <djoepramono@gmail.com>
2020-07-26 20:28:42 +1000

    update ParseResult to be 2 tier

--- src/command_parser.rs
+++ src/command_parser.rs
@@ -1,23 +1,41 @@
 use std::fmt;
 
-pub enum ParseResult {
-  AppError { message: &'static str},
-  Command,
+pub struct AppError {
+  message: &'static str,
 }
 
-impl fmt::Display for ParseResult {
+pub enum Command {
+  Cards,
+  Boards
+}
+
+pub enum ParseResult<E, A> {
+  Error(E),
+  Success(A),
+}
+
+impl fmt::Display for ParseResult<AppError, Command> {
   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-      write!(f, "{:?}",  match self {
-        ParseResult::AppError { message } => message,
-        ParseResult::Command => "command",
+      write!(f, "{:?}", match self {
+        ParseResult::Error(error) => error.message,
+        ParseResult::Success(command) => match command {
+          Command::Cards => "cards",
+          Command::Boards => "boards",
+        },
     })
   }
 }
 
-pub fn parse(input: &str) -> ParseResult {
-  println!("debug: '{}'", input);
+fn show_usage() -> &'static str {
+  "USAGE: krello <command> \n
+  where command: Cards | Boards
+  "
+}
+
+pub fn parse(input: &str) -> ParseResult<AppError, Command> {
   match input {
-    "cards" => ParseResult::Command,
-    _ => ParseResult::AppError { message : "unknown command" }
+    "cards" => ParseResult::Success( Command::Cards),
+    "boards" => ParseResult::Success( Command::Boards),
+    _ => ParseResult::Error(AppError { message : show_usage() }),
   }
 }
