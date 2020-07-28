1b75b3c86164a1d7e69fc2cbff7895cde371bda8
Djoe Pramono <djoepramono@gmail.com>
2020-07-28 23:31:56 +1000

    first attempt to do http request

--- README.md
+++ README.md
@@ -12,3 +12,26 @@ after build
 ```
 krello <params>
 ```
+
+# Development
+
+Sometimes you will need to install specific package in your build machine, for example in Ubuntu, the following package needs to be installed to be able to use `reqwest`
+
+```
+sudo apt install libssl-dev
+```
+
+# Common Problem
+
+## Blocking error
+```
+Blocking waiting for file lock on package cache
+```
+
+This usually happens during an installation of a new dependencies. VS Code plugin will try to install the dependency straight away and sometimes this take time. Easier if you install via command line
+
+So
+1. Stop rust server in VS Code
+2. `rm ~/.cargo/.package-cache`
+3. Make sure your `cargo.toml` is set correctly
+4, run `cargo build` in your terminal
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,14 @@ use std::env;
 pub mod command_parser;
 use command_parser::*;
 use either::Either;
+// use reqwest::blocking;
+use error_chain::error_chain;
 
 fn main() {
   let args: Vec<String> = env::args().collect();
   let input = &args[1];
+
+  let _ = send_reqwest();
 
   let clean_input = input.trim();
   let output = parse(clean_input);
@@ -18,4 +22,25 @@ fn main() {
 fn print_usage() -> () {
   println!("USAGE:");
   println!("krello <command> where command: Cards | Boards");
+}
+
+// to bubble error ...
+error_chain! {
+  foreign_links {
+      Io(std::io::Error);
+      HttpRequest(reqwest::Error);
+  }
+}
+
+fn send_reqwest() -> Result<()> {
+  let res = reqwest::blocking::get("http://httpbin.org/get")?;
+  // let mut body = String::new();
+  // res.read_to_string(&mut body)?;
+  let body = res.text()?;
+
+  // println!("Status: {}", res.status());
+  // println!("Headers:\n{:#?}", res.headers());
+  println!("Body:\n{}", body);
+
+  Ok(())
 }
